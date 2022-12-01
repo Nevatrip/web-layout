b75e1a6cd9c4596cf4847168ef4c00ca175111a3
Egor Golikov <elai.asti@gmail.com>
2022-12-01 23:28:59 +0700

    Создал сборку и базовые стили

---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        use: 'html-loader',
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['autoprefixer'],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(jpe?g|png|gif|ico)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[hash][ext]',
+        },
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset',
+        generator: {
+          filename: 'assets/icons/[hash][ext]',
+        },
+        use: 'svgo-loader',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[hash][ext]',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+};
+
+export default (env, argv) => {
+  if (argv.mode === 'development') {
+    config.output.filename = '[name].js';
+    config.devtool = 'source-map';
+  }
+
+  if (argv.mode === 'production') {
+    config.output.filename = '[name].[contenthash].js';
+  }
+
+  return config;
+};
