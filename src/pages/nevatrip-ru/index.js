81438c93d5e2bab32a4f7b619856de03af091025
Damir021 <k.damir021@gmail.com>
2022-12-07 21:42:47 +0600

    Добавил кнопку прокрутки вверх

--- src/pages/nevatrip-ru/index.js
+++ src/pages/nevatrip-ru/index.js
@@ -2,6 +2,7 @@ import '../../scss/pages/nevatrip-ru.scss';
 import { dropDownMenu } from './../../js/drop-down-menu/drop-down-menu.js';
 import Topbar from '../../js/topbar.js';
 import Navbar from '../../js/navbar.js';
+import ButtonScrollTop from '../../js/button-scroll-top.js';
 
 new Topbar(document.querySelector('.dke_topbar'));
 new Navbar(document.querySelector('.dke_navbar'));
@@ -9,6 +10,9 @@ new Navbar(document.querySelector('.dke_navbar'));
 function ready() {
   // выпадающее меню (desktop), бургер-меню (mobile)
   dropDownMenu();
+
+  // кнопка прокрутки страницы вверх
+  new ButtonScrollTop('.dke_button-scroll-top');
 }
 
 function load() {
