014e81eff6330d90ef2e99fd65942788f4c4d948
ZharikovaES <z.kate2000@gmail.com>
2022-12-21 20:01:03 +0300

    дописаны стили, частично написаны скрипты к выпадающим спискам

--- src/pages/excursion/index.js
+++ src/pages/excursion/index.js
@@ -3,6 +3,7 @@ import '../../scss/pages/nevatrip-ru.scss';
 import { dropDownMenu } from './../../js/drop-down-menu.js';
 import Topbar from '../../js/topbar.js';
 import Navbar from '../../js/navbar.js';
+import { attractionList } from './../../js/attraction-list.js';
 import ButtonScrollTop from '../../js/button-scroll-top.js';
 
 new Topbar(document.querySelector('.dke_topbar'));
@@ -10,6 +11,9 @@ new Topbar(document.querySelector('.dke_topbar'));
 function ready() {
   // выпадающее меню (desktop), бургер-меню (mobile)
   dropDownMenu();
+
+  // список достопримечательностей
+  attractionList();
 
   // кнопка прокрутки страницы вверх
   // new ButtonScrollTop('.dke_button-scroll-top');
