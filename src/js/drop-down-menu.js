9337756c41bf96e8dc36fa25bc522ca5114ca3de
ZharikovaES <z.kate2000@gmail.com>
2022-12-26 12:04:05 +0300

    поправлен код в скрипте меню

--- src/js/drop-down-menu.js
+++ src/js/drop-down-menu.js
@@ -14,14 +14,6 @@ export function dropDownMenu() {
   btnTopHeader?.addEventListener('click', showMenu);
 
   // нажатие на бургер-кнопку блока navbar для открытия/закртия меню
-  const itemsDropDownMenuMobile = document.querySelectorAll(
-    '.dke_item-drop-down-mobile'
-  );
-
-  // нажатие на кнопку блока topbar для открытия/закртия меню
-  btnTopHeader?.addEventListener('click', showMenu);
-
-  // нажатие на кнопку блока navbar для открытия/закртия меню
   btnNavbar?.addEventListener('click', showMenu);
 
   // открытие/закрытие внутренних списков меню на desktop
@@ -54,9 +46,6 @@ window.addEventListener('resize', () => {
 
     // если бургер-кнопок нет или они скрыты, закрывать меню
     if (
-      !btnTopHeader && 
-      (!btnNavbar || getComputedStyle(btnNavbar, null).display === 'none')) 
-
       !btnTopHeader &&
       (!btnNavbar || getComputedStyle(btnNavbar, null).display === 'none')
     )
@@ -74,9 +63,6 @@ function showMenu() {
   
   document.body.classList.toggle('js_dke_active-menu');
   
-  [btnTopHeader, btnNavbar].forEach(function(element) {
-  document.body.classList.toggle('_active-menu');
-
   [btnTopHeader, btnNavbar].forEach(function (element) {
     if (element) {
       toggleAttr(element, 'aria-expanded');
@@ -85,18 +71,6 @@ function showMenu() {
   });
 
   if (this) this.disabled = false;
-}
-
-// открытие/закртие выпадающих списков
-function showSublist(element) {
-  this.classList.add('_disabled');
-
-  if (element) {
-    element.classList.toggle('_active');
-    toggleAttr(element, 'aria-expanded');
-  }
-
-  this.classList.remove('_disabled');
 }
 
 // меняем положение мобильного меню (по вертикали) в зависимости от высоты navbar
