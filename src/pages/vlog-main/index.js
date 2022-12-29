f550738a0a826dfc9a069b36643d2c5a09d4ea7d
Damir021 <k.damir021@gmail.com>
2022-12-29 09:16:02 +0600

    Написал скрипт для модалки влога

--- src/pages/vlog-main/index.js
+++ src/pages/vlog-main/index.js
@@ -1,4 +1,6 @@
 import '../../scss/pages/vlog-main.scss';
+import Modal from '../../js/modal.js';
+
 
 import { dropDownMenu } from './../../js/drop-down-menu.js';
 import Topbar from '../../js/topbar.js';
@@ -8,6 +10,22 @@ new Topbar(document.querySelector('.dke_topbar'));
 function ready() {
   // выпадающее меню (desktop), бургер-меню (mobile)
   dropDownMenu();
+
+  // Модальные окна влога
+  const vlogModals = document.querySelectorAll('[data-vlog-modal]');
+  vlogModals.forEach(modal => {
+    const modalId = modal.dataset.vlogModal;
+    const modalInstance = new Modal(modal);
+
+    const modalOpenButtons = document.querySelectorAll(`[data-modal-open="${modalId}"]`);
+    modalOpenButtons.forEach(button => {
+      button.addEventListener('click', (e)=> {
+        e.preventDefault();
+        modalInstance.openModal();
+      })
+    })
+    
+  });
 }
 
 function load() {
